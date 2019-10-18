88221de00e08c8d3ced15d7e5859325e381668de
Khwilo Kabaka <khwilowatai@gmail.com>
2019-10-18 20:41:03 +0300

    Render the header with its background image

--- src/app/movies/movie-detail/movie-detail.component.ts
+++ src/app/movies/movie-detail/movie-detail.component.ts
@@ -12,6 +12,8 @@ export class MovieDetailComponent implements OnInit {
   movie: any;
   cast: any;
   crew: any;
+  BASE_IMAGE_URL = 'http://image.tmdb.org/t/p/w1280/';
+  backdropPath: string;
 
   constructor(
     private movieDataService: MovieDataService,
@@ -23,11 +25,22 @@ export class MovieDetailComponent implements OnInit {
       const movieID = +params.get('id');
       this.movieDataService.getMovie(movieID).subscribe(data => {
         this.movie = data;
+        this.backdropPath = data['backdrop_path'];
       });
       this.movieDataService.getCredits(movieID).subscribe(data => {
         this.cast = data['cast'];
         this.crew = data['crew'];
       });
     });
+  }
+
+  setBackgroundImage() {
+    const styles = {
+      'background-size': 'cover',
+      'background-image': `url(${this.BASE_IMAGE_URL}${this.backdropPath})`,
+      'background-color': '#3c3c3c',
+      'background-repeat': 'no-repeat'
+    };
+    return styles;
   }
 }
